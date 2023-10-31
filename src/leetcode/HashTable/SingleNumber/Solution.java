79122b92b600a59f33b12973787fc2736fc7c350
Valeriy_Orlov <orlov332@gmail.com>
2023-10-31 23:31:40 +0100

    train: Hash table training session

---
+++ src/leetcode/HashTable/SingleNumber/Solution.java
@@ -0,0 +1,11 @@
+package leetcode.HashTable.SingleNumber;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
