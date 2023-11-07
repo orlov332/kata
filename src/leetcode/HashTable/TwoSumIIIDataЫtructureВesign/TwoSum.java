ee68ac6b676a648de155e35c924c27cd5e77e7f1
Valeriy_Orlov <orlov332@gmail.com>
2023-11-07 18:35:19 +0100

    train: Hash table training session

---
+++ src/leetcode/HashTable/TwoSumIIIDataЫtructureВesign/TwoSum.java
@@ -0,0 +1,24 @@
+package leetcode.HashTable.TwoSumIIIDataЫtructureВesign;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+
+    private final Map<Integer, Integer> numCount = new HashMap<>(10000);
+
+    public void add(int number) {
+        numCount.compute(number, (k, v) -> v == null ? 1 : v + 1);
+    }
+
+    public boolean find(int value) {
+        for (Map.Entry<Integer, Integer> entry : numCount.entrySet()) {
+            int num = entry.getKey();
+            int count = entry.getValue();
+            if (numCount.containsKey(value - num) && (value - num != num || count > 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
